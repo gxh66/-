29ceb1969c32f709547830081468cd9528b2bec9
GXH66 <486888971@qq.com>
2023-03-13 12:14:30 +0800

    The first week!

---
+++ TestFirst.java
@@ -0,0 +1,19 @@
+public class TestFirst {
+    public static void main(String[] args) {
+        int[] sum = {2, 7, 11, 19, 20, 25};
+        int index = -1;
+        int index2 = -1;
+        int target = 9;
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = 0; j < sum.length; j++) {
+                if (sum[i] + sum[j] == target) {
+                    index = i;
+                    index2 = j;
+                    System.out.println("index:" + index);
+                    System.out.println("index2:" + index2);
+                }
+            }
+        }
+    }
+}
+}
